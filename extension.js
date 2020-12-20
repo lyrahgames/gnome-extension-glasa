f687649264e2347db6ca2f67244013cfad4fd4a7
lyrahgames <lyrahgames@mailbox.org>
2020-12-20 22:08:09 +0100

    Draw two comic eyes

--- extension.js
+++ extension.js
@@ -25,56 +25,72 @@ class Extension {
     let indicatorName = `${Me.metadata.name} Indicator`;
     this._indicator = new PanelMenu.Button(0.0, indicatorName, false);
 
-
-    log(Panel.PANEL_SIZE);
-
-    const MARGIN = 2;
-    // let size = Panel.PANEL_ICON_SIZE - 2 * MARGIN;
     let size = Panel.PANEL_ICON_SIZE;
-    // let size = 20;
-    let icon = new St.DrawingArea({width: size, height: size});
+    let icon = new St.DrawingArea({width: 2 * size, height: size});
     icon._repaint_handler = icon.connect('repaint', Lang.bind(icon, function() {
-      let halfsize = icon.width / 2;
+      let halfsize = icon.height / 2;
       let [area_x, area_y] = icon.get_transformed_position();
       let [mouse_x, mouse_y, mask] = global.get_pointer();
-      mouse_x -= area_x + halfsize;
-      mouse_y -= area_y + halfsize;
 
       const EYE_LINE_WIDTH = 1.5;
-      const RELIEF_FACTOR = 6;
+      const RELIEF_FACTOR = 30;
+      const RELIEF_FACTOR_BOUND = 0.7;
       const IRIS_MOVE = 0.66;
-      const IRIS_SIZE = 0.55;
-      const PUPIL_MOVE = 0.44;
-      const PUPIL_SIZE = 0.15;
+      const IRIS_SIZE = 0.5;
+      const EYEBROW_SCALE = 1.4;
 
-      let eye_rad = halfsize - EYE_LINE_WIDTH / 2;
+      let eye_radius = 2 * halfsize / (1 + EYEBROW_SCALE);
+      let eyebrow_radius = EYEBROW_SCALE * eye_radius;
+      eye_radius -= EYE_LINE_WIDTH / 2;
+      eyebrow_radius -= EYE_LINE_WIDTH / 2;
+      let center_y = halfsize * (EYEBROW_SCALE + 1) / 2;
+      let left_center_x = 2 * halfsize - eye_radius;
+      let right_center_x = 2 * halfsize + eye_radius;
+
+      mouse_x -= area_x + 2 * halfsize;
+      mouse_y -= area_y + center_y;
+
       let factor = Math.sqrt(mouse_x * mouse_x + mouse_y * mouse_y) /
-          (RELIEF_FACTOR * eye_rad);
-      if (factor > 1) factor = 1;
-      let iris_move = eye_rad * IRIS_MOVE * factor;
+          (RELIEF_FACTOR * eye_radius);
+      if (factor > RELIEF_FACTOR_BOUND) factor = RELIEF_FACTOR_BOUND;
+      let iris_move = eye_radius * IRIS_MOVE * factor;
+
+      // Get and set up the Cairo context.
       let cr = icon.get_context();
       let theme_node = icon.get_theme_node();
       Clutter.cairo_set_source_color(cr, theme_node.get_foreground_color());
 
-      cr.translate(halfsize, halfsize);
-
       cr.setLineWidth(EYE_LINE_WIDTH);
-      cr.arc(0, 0, eye_rad, 0, 2 * Math.PI);
-      cr.stroke();
+      cr.save();
 
-      cr.setLineWidth(0);  // iris and pupil are filled
+      // Draw the left eye.
+      cr.translate(left_center_x, center_y);
+      cr.arc(0, 0, eye_radius, 0, 2 * Math.PI);
+      cr.stroke();
+      // Draw the left eyebrow.
+      cr.arc(0, 0, eyebrow_radius, 5 * Math.PI / 4, 6.5 * Math.PI / 4);
+      cr.stroke();
+      // Draw the left iris/pupil.
       cr.rotate(Math.PI / 2 - Math.atan2(mouse_x, mouse_y));
       cr.translate(iris_move, 0);
       cr.scale(Math.cos(factor), 1);
+      cr.arc(0, 0, eye_radius * IRIS_SIZE, 0, 2 * Math.PI);
+      cr.fill();
+      cr.restore();
 
-      cr.arc(0, 0, eye_rad * IRIS_SIZE, 0, 2 * Math.PI);
-      cr.translate(iris_move * PUPIL_MOVE, 0.0);
-      cr.newSubPath();
-      cr.arc(0, 0, eye_rad * PUPIL_SIZE, 0, 2 * Math.PI);
-      cr.setFillRule(Cairo.FillRule.EVEN_ODD);
-      cr.fillPreserve();
-
-      cr.save();
+      // Draw the right eye;
+      cr.translate(right_center_x, center_y);
+      cr.arc(0, 0, eye_radius, 0, 2 * Math.PI);
+      cr.stroke();
+      // Draw the right eyebrow.
+      cr.arc(0, 0, eyebrow_radius, 5.5 * Math.PI / 4, 7 * Math.PI / 4);
+      cr.stroke();
+      // Draw the right iris/pupil.
+      cr.rotate(Math.PI / 2 - Math.atan2(mouse_x, mouse_y));
+      cr.translate(iris_move, 0);
+      cr.scale(Math.cos(factor), 1);
+      cr.arc(0, 0, eye_radius * IRIS_SIZE, 0, 2 * Math.PI);
+      cr.fill();
       cr.restore();
     }));
     icon._update_handler = Mainloop.timeout_add(50, Lang.bind(icon, function() {
@@ -92,7 +108,7 @@ class Extension {
 
     this._indicator.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
     this._indicator.menu.addAction(
-        _('Preferences'),
+        _('I am with you. Keep on!'),
         event => {
 
         });
