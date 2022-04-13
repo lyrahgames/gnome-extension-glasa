cf244c54ac05040747efe7dc7ad10e6555e4bef1
lyrahgames <lyrahgames@mailbox.org>
2022-04-13 14:28:05 +0200

    Update extension to support GNOME version 42

--- prefs.js
+++ prefs.js
@@ -1,106 +1,88 @@
 'use strict';
 
-const {Gio, Gtk, GObject} = imports.gi;
+const { Adw, Gio, Gtk } = imports.gi;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
-// Currently, this is the interface for GNOME Shell version 40 and 41.
-// It will likely change for the next release of GNOME Shell version 42.
-//
-// There are too few settings right now.
-// Hence, GtkBuilder does not seem to be appropriate.
-const GlasaSettingsWidget =
-    GObject.registerClass(class GlasaSettingsWidget extends Gtk.ScrolledWindow {
-      _init() {
-        super._init({
-          hscrollbar_policy : Gtk.PolicyType.NEVER,
-        });
-
-        // Retrieve the extension's settings state.
-        this.settings =
-            ExtensionUtils.getSettings('org.gnome.shell.extensions.glasa');
-
-        // Main box in window for vertical alignment.
-        const box = new Gtk.Box({
-          orientation : Gtk.Orientation.VERTICAL,
-          halign : Gtk.Align.CENTER,
-          spacing : 12,
-          margin_top : 36,
-          margin_bottom : 36,
-          margin_start : 36,
-          margin_end : 36,
-        });
-        this.set_child(box);
-
-        box.append(new Gtk.Label({
-          label : 'Top Panel Box and Location in Box:',
-        }));
-
-        // Horizontal box for icon position in top panel.
-        const hbox = new Gtk.Box({
-          orientation : Gtk.Orientation.HORIZONTAL,
-          halign : Gtk.Align.CENTER,
-          spacing : 12,
-          margin_top : 12,
-          margin_bottom : 12,
-          margin_start : 36,
-          margin_end : 36,
-        });
-        box.append(hbox);
-
-        // Create group of toggle buttons to set the panel box.
-        let left_button = new Gtk.ToggleButton({label : 'Left'});
-        let center_button = new Gtk.ToggleButton({label : 'Center'});
-        center_button.set_group(left_button);
-        let right_button = new Gtk.ToggleButton({label : 'Right'});
-        right_button.set_group(left_button);
-
-        // Set the initial state of the toggle button group.
-        const init_panel_box = this.settings.get_int('panel-box');
-        if (init_panel_box == 0)
-          left_button.set_active(true);
-        else if (init_panel_box == 1)
-          center_button.set_active(true);
-        else if (init_panel_box == 2)
-          right_button.set_active(true);
-
-        // Make toggle button update settings state.
-        left_button.connect('toggled',
-                            () => { this.settings.set_int('panel-box', 0); });
-        center_button.connect('toggled',
-                              () => { this.settings.set_int('panel-box', 1); });
-        right_button.connect('toggled',
-                             () => { this.settings.set_int('panel-box', 2); });
-
-        // Add the button to the GUI.
-        hbox.append(left_button);
-        hbox.append(center_button);
-        hbox.append(right_button);
-
-        // Inside each panel box, the order of indicators should be provided.
-        // Use a spin button with the corresponding initial value.
-        const init_panel_box_location =
-            this.settings.get_int('panel-box-location');
-        let number_button = new Gtk.SpinButton();
-        number_button.set_adjustment(new Gtk.Adjustment({
-          lower : 0,
-          upper : 100,
-          value : init_panel_box_location,
-          step_increment : 1,
-          page_increment : 1,
-          page_size : 0,
-        }));
-        // Let the spin button update the settings state.
-        number_button.connect('value-changed', () => {
-          this.settings.set_int('panel-box-location',
-                                number_button.get_adjustment().value);
-        });
-        // Add it to the GUI.
-        hbox.append(number_button);
-      }
-    });
-
 function init() {}
 
-function buildPrefsWidget() { return new GlasaSettingsWidget(); }
+// Currently, the GUI is simple enough to not use the builder.
+function fillPreferencesWindow(window) {
+  const settings =
+    ExtensionUtils.getSettings('org.gnome.shell.extensions.glasa');
+
+  window.set_size_request(600, 200);
+  window.set_default_size(800, 250);
+
+  const page = new Adw.PreferencesPage();
+  window.add(page);
+
+  const group = new Adw.PreferencesGroup({ title: 'General' });
+  page.add(group);
+
+  const row = new Adw.ActionRow({
+    title: 'Top Panel Location',
+    subtitle: 'Box and Order',
+  });
+  group.add(row);
+
+  // Create group of toggle buttons to set the panel box.
+  // Make sure to set the vertical alignment.
+  // So, the buttons will not get stretched vertically.
+  const left_button =
+    new Gtk.ToggleButton({ label: 'Left', valign: Gtk.Align.CENTER });
+  const center_button =
+    new Gtk.ToggleButton({ label: 'Center', valign: Gtk.Align.CENTER });
+  const right_button =
+    new Gtk.ToggleButton({ label: 'Right', valign: Gtk.Align.CENTER });
+  center_button.set_group(left_button);
+  right_button.set_group(left_button);
+
+  // Set the initial state of the toggle button group.
+  const init_panel_box = settings.get_int('panel-box');
+  if (init_panel_box == 0)
+    left_button.set_active(true);
+  else if (init_panel_box == 1)
+    center_button.set_active(true);
+  else if (init_panel_box == 2)
+    right_button.set_active(true);
+
+  // Make toggle buttons update the settings state.
+  left_button.connect('toggled', () => {
+    settings.set_int('panel-box', 0);
+  });
+  center_button.connect('toggled', () => {
+    settings.set_int('panel-box', 1);
+  });
+  right_button.connect('toggled', () => {
+    settings.set_int('panel-box', 2);
+  });
+
+  // Add the toggle buttons to the action row.
+  row.add_suffix(left_button);
+  row.add_suffix(center_button);
+  row.add_suffix(right_button);
+
+  // Inside each panel box, the order of indicators should be provided.
+  // Use a spin button with the corresponding initial value.
+  const init_panel_box_location = settings.get_int('panel-box-location');
+  const number_button = new Gtk.SpinButton({ valign: Gtk.Align.CENTER });
+  number_button.set_adjustment(new Gtk.Adjustment({
+    lower: 0,
+    upper: 100,
+    value: init_panel_box_location,
+    step_increment: 1,
+    page_increment: 1,
+    page_size: 0,
+  }));
+
+  // Let the spin button update the settings state.
+  number_button.connect('value-changed', () => {
+    settings.set_int(
+      'panel-box-location', number_button.get_adjustment().value);
+  });
+
+  // Add it to the GUI.
+  row.add_suffix(number_button);
+}
