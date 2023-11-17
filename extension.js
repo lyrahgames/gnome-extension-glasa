b94bc27d4c3bbbd296db825c9c816de61b7092ea
Simon Plakolb <s.plakolb@gmail.com>
2023-11-17 10:53:47 +0100

    Reduce relief factor to 2 eye radii

--- extension.js
+++ extension.js
@@ -105,7 +105,7 @@ export default class GlasaExtension extends Extension {
     let mouse_x_r = mouse_x;
 
     const EYE_LINE_WIDTH = 1.5;
-    const RELIEF_FACTOR = 30;
+    const RELIEF_FACTOR = 2;
     const RELIEF_FACTOR_BOUND = 0.7;
     const IRIS_MOVE = 0.66;
     const IRIS_SIZE = 0.5;
